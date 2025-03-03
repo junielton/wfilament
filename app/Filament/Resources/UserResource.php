90517665ed88ddf7ab70093daa007b3786a10bb3
junielton <junyellton@gmail.com>
2025-03-03 00:24:38 +0000

    feat: create UserResource with form and table definitions for user management

---
+++ app/Filament/Resources/UserResource.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\UserResource\Pages;
+use App\Filament\Resources\UserResource\RelationManagers;
+use App\Models\User;
+use Filament\Forms;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class UserResource extends Resource
+{
+    protected static ?string $model = User::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                TextInput::make('name')
+                    ->label('Name')
+                    ->placeholder('John Doe')
+                    ->required()
+                    ->autofocus(),
+                TextInput::make('email')
+                    ->label('Email')
+                    ->type('email')
+                    ->unique(User::class, 'email')
+                    ->placeholder('email@example.com')
+                    ->required(),
+                TextInput::make('password')
+                    ->label('Password')
+                    ->type('password')
+                    ->placeholder('Password')
+                    ->required(),
+                TextInput::make('password_confirmation')
+                    ->label('Confirm Password')
+                    ->type('password')
+                    ->placeholder('Confirm Password')
+                    ->required(),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('id'),
+                TextColumn::make('name'),
+                TextColumn::make('email'),
+                TextColumn::make('email_verified_at'),
+                TextColumn::make('created_at'),
+                TextColumn::make('updated_at'),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListUsers::route('/'),
+            'create' => Pages\CreateUser::route('/create'),
+            'edit' => Pages\EditUser::route('/{record}/edit'),
+        ];
+    }
+}
